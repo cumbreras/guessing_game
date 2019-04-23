5f42e34e72a66b453ae73f59fdcdf5440de74d0c
Edgar Cumbreras <edgarcumbrerastirado@gmail.com>
2019-04-23 23:11:22 +0200

    Moving logic within the game struct

--- src/core/game.rs
+++ src/core/game.rs
@@ -1,8 +1,9 @@
-use super::super::helpers::{random, user_input};
+use super::super::helpers::{mtc, parser, random, user_input};
 use super::player;
 
 const INITIAL_ROUNDS: i32 = 0;
 const DEFAULT_PLAYERS: i32 = 1;
+const TURNS: i32 = 8;
 
 pub struct Game {
     pub players: Vec<player::Player>,
@@ -13,28 +14,65 @@ pub struct Game {
 impl Game {
     pub fn new() -> Game {
         println!("New Game!");
-        let number_of_players = user_input::get("How many players?");
-        let number_of_players: i32 = match number_of_players.trim().parse() {
-            Ok(num) => num,
-            Err(_) => DEFAULT_PLAYERS,
-        };
-
-        println!("We are playing {}", number_of_players);
-
         Game {
-            players: Game::build_players(number_of_players),
+            players: Game::build_players(),
             rounds: INITIAL_ROUNDS,
             secret_number: random::number(),
         }
     }
 
-    pub fn build_players(number_of_players: i32) -> Vec<player::Player> {
+    fn build_players() -> Vec<player::Player> {
         let mut players: Vec<player::Player> = vec![];
+        let number_of_players = Game::get_number_of_players();
 
         for _ in 0..number_of_players {
             players.push(player::Player::new());
         }
 
         players
+    }
+
+    fn get_number_of_players() -> i32 {
+        let number_of_players = user_input::get("How many players?");
+        return match number_of_players.trim().parse() {
+            Ok(num) => {
+                println!("We are playing {}", number_of_players);
+                num
+            }
+            Err(_) => DEFAULT_PLAYERS,
+        };
+    }
+
+    fn turn(self, p: &mut player::Player) -> bool {
+        let guess = user_input::get(&format!("Give me a guess: {}", p.name));
+        let guess: Option<i32> = parser::number(guess);
+
+        return match guess {
+            None => false,
+            Some(guess) => {
+                if mtc::number_cmp(guess, &self.secret_number) {
+                    p.won();
+                    println!(
+                        "{} has won this turn, now has: {} points",
+                        p.name,
+                        p.score()
+                    );
+                    return true;
+                }
+                return false;
+            }
+        };
+    }
+
+    pub fn start(mut self) {
+        for turn in 0..TURNS {
+            println!("Turn number: {}", turn);
+
+            for p in self.players.iter_mut() {
+                *self.turn(p);
+            }
+        }
+
+        println!("You lost");
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,35 +2,7 @@ mod core;
 mod helpers;
 
 use self::core::game;
-use helpers::{mtc, parser, user_input};
 
 fn main() {
-    const TURNS: i32 = 8;
-    let mut current_game = game::Game::new();
-
-    for turn in 0..TURNS {
-        println!("Turn number: {}", turn);
-
-        for p in current_game.players.iter_mut() {
-            let guess = user_input::get(&format!("Give me a guess: {}", p.name));
-            let guess: Option<i32> = parser::number(guess);
-
-            match guess {
-                None => continue,
-                Some(guess) => {
-                    if mtc::number_cmp(guess, &current_game.secret_number) {
-                        p.won();
-                        println!(
-                            "{} has won this turn, now has: {} points",
-                            p.name,
-                            p.score()
-                        );
-                        return;
-                    }
-                }
-            }
-        }
-    }
-
-    println!("You lost");
+    game::Game::new().start()
 }
